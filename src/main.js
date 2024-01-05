67118c5e240a1bf6e05fa0dd51a6c134dce1cc30
AdrianGerman <adrian.german1019@gmail.com>
2024-01-05 10:51:28 -0600

    post

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const API_URL_RANDOM =
   "https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_5UT9xBpqUI9PwZ1q3zzqzQuVXSSPJtPsoTZWgncgiAM1GpeUDKOZSFG8qRnWJHhI";
 
 const API_URL_FAVORITES =
-  "https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_5UT9xBpqUI9PwZ1q3zzqzQuVXSSPJtPsoTZWgncgiAM1GpeUDKOZSFG8qRnWJHhI";
+  "https://api.thecatapi.com/v1/favourites?api_key=live_5UT9xBpqUI9PwZ1q3zzqzQuVXSSPJtPsoTZWgncgiAM1GpeUDKOZSFG8qRnWJHhI";
 
 const spanError = document.getElementById("error");
 
@@ -25,7 +25,7 @@ async function loadRandomMichis() {
   }
 }
 
-async function loadFavoritesMichis() {
+async function loadFavouritesMichis() {
   const res = await fetch(API_URL_FAVORITES);
   const data = await res.json();
   console.log("Favoritos");
@@ -36,5 +36,25 @@ async function loadFavoritesMichis() {
   }
 }
 
+async function saveFavouriteMichis() {
+  const res = await fetch(API_URL_FAVORITES, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      image_id: "dje",
+    }),
+  });
+  const data = await res.json();
+
+  console.log("Save");
+  console.log(res);
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  }
+}
+
 loadRandomMichis();
-loadFavoritesMichis();
+loadFavouritesMichis();
