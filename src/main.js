89c6aa3d3edfa3699a530dde8d59db831e604894
AdrianGerman <adrian.german1019@gmail.com>
2024-01-06 12:57:14 -0600

    dynamic DOM manipulation

--- src/main.js
+++ src/main.js
@@ -18,10 +18,17 @@ async function loadRandomMichis() {
     const img1 = document.getElementById("img1");
     const img2 = document.getElementById("img2");
     const img3 = document.getElementById("img3");
+    const btn1 = document.getElementById("btn1");
+    const btn2 = document.getElementById("btn2");
+    const btn3 = document.getElementById("btn3");
 
     img1.src = data[0].url;
     img2.src = data[1].url;
     img3.src = data[2].url;
+
+    btn1.onclick = () => saveFavouriteMichis(data[0].id);
+    btn2.onclick = () => saveFavouriteMichis(data[1].id);
+    btn3.onclick = () => saveFavouriteMichis(data[2].id);
   }
 }
 
@@ -33,17 +40,34 @@ async function loadFavouritesMichis() {
 
   if (res.status !== 200) {
     spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  } else {
+    data.forEach((michi) => {
+      const section = document.getElementById("favoriteMichis");
+      const article = document.createElement("article");
+      const img = document.createElement("img");
+      const btn = document.createElement("btn");
+      const btnText = document.createTextNode(
+        "Sacar al michi de los favoritos"
+      );
+
+      img.src = michi.image.url;
+      img.width = 150;
+      btn.appendChild(btnText);
+      article.appendChild(img);
+      article.appendChild(btn);
+      section.appendChild(article);
+    });
   }
 }
 
-async function saveFavouriteMichis() {
+async function saveFavouriteMichis(id) {
   const res = await fetch(API_URL_FAVORITES, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      image_id: "dje",
+      image_id: id,
     }),
   });
   const data = await res.json();
